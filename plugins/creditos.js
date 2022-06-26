8076edb77308f3dd3861616cb6335a197a814efb
GataDios <105464156+GataDios@users.noreply.github.com>
2022-06-25 22:47:44 -0400

    Update creditos.js

--- plugins/creditos.js
+++ plugins/creditos.js
@@ -2,14 +2,14 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╭═════• ೋ•✧๑♡๑✧•ೋ •═════╮ 
 
-*Paragua-Bor tiene funciones que pertenezcan a las siguentes Empresa(s)|Organizacion(es)|Persona(s):*
+*Paragua-Bor tiene funcion que pertenecen a Matias(s)|Organizacion(es)|Persona(s):*
 
 _*MATIAS_*
  
 ╰═════• ೋ•✧๑♡๑✧•ೋ •═════╯
 `.trim() 
 //¡GRACIAS!
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🇵🇾Paragua🇵🇾', 'status@broadcast')
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', 'Paragua - ', 'status@broadcast')
 }
 handler.command = /^(creditos|CREDITOS|Creditos|Credito|Crédito)$/i
 
